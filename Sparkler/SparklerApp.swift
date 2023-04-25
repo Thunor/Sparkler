c2824ecad9f153c09ae723542cdd3385f5ca22e1
Eric Freitas <efreitos@gmail.com>
2023-04-25 01:11:28 -0400

    Initial Commit

---
+++ Sparkler/SparklerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparklerApp.swift
+//  Sparkler
+//
+//  Created by Eric Freitas on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SparklerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
